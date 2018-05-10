79af25983730ebce0dda731dcc99ed734d14872c
chicrock <jungsungrak@gmail.com>
2018-05-10 18:17:21 +0900

    Theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import styled, { injectGlobal, css } from "styled-components";
+import styled, { injectGlobal, css, ThemeProvider } from "styled-components";
+
+import theme from "./theme";
 
 injectGlobal`
 body{
@@ -8,6 +10,14 @@ body{
 }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+//${awesomeCard};
+/*
 /// add this in shared files.
 const awesomeCard = css`
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
@@ -16,12 +26,6 @@ const awesomeCard = css`
   padding: 20px;
 `;
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  ${awesomeCard};
-  background-color: pink;
-`;
 
 const Input = styled.input.attrs({
   required: true,
@@ -30,13 +34,32 @@ const Input = styled.input.attrs({
   border-radius: 5px;
   ${awesomeCard};
 `;
+*/
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="Hello" />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
